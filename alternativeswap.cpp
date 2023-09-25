103a8378677dc016a05389f0ac506ef821fe9480
Mayur Raj <mayur.13519011622@ipu.ac.in>
2023-09-25 10:37:37 +0530

    Final commit

--- alternativeswap.cpp
+++ alternativeswap.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 using namespace std;
-int main(){
-    cout<<"hi";
-    return 0;
+
+void print_array(int arr[], int size){
+    for (int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
 }
+
+void alt_swap(int arr[], int size){
+    for (int i=0;i<=size;i+=2){
+        if (i+1<size){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+}
+int main(){
+
+    int even_arr[10] = {5,8,4,9,5,2,1,6,0,3};
+    int odd_arr[9] = {4,8,7,5,6,9,2,3,1};
+
+    alt_swap(even_arr,10);
+    print_array(even_arr,10);
+
+    alt_swap(odd_arr,9);
+    print_array(odd_arr,9);
+
+    return 0;
+
+}
+
