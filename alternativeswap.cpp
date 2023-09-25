fc05de259c3fef1d4bfc5a1b6081de7eb168b59d
Mayur Raj <mayur.13519011622@ipu.ac.in>
2023-09-25 10:41:31 +0530

    Without using swap function

--- alternativeswap.cpp
+++ alternativeswap.cpp
@@ -11,7 +11,10 @@ void print_array(int arr[], int size){
 void alt_swap(int arr[], int size){
     for (int i=0;i<=size;i+=2){
         if (i+1<size){
-            swap(arr[i],arr[i+1]);
+            int temp;
+            temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
         }
     }
 }
