7b3848f2841c577a4852eb0135615c76b88b8ef9
Mayur Raj <mayur.13519011622@ipu.ac.in>
2023-10-13 23:17:21 +0530

    arrayinsertion

--- arrayinsertion.cpp
+++ arrayinsertion.cpp
@@ -2,27 +2,30 @@
 using namespace std;
 int main(){
     cout<<"array insertion"<<endl;
-    int array[50],size,pos;
-    cout<<"enter size of array ";
+    int array[50],size,pos,elem;
+    cout<<"enter size of array"<<endl;
     cin>>size;
-    cout<<"enter elements in array ";
+    cout<<"enter elements in array"<<endl;
     for (int i=0;i<size;i++){
         cin>>array[i];
     }
-    cout<<"enter the position you want to delete ";
+    cout<<"enter the position you want to insert ";
     cin>>pos;
     if (pos>size || pos<=0)
     {
         cout<<"invalid position, please enter  a different position again ";
         cin>>pos;
     }
-    for (int i=pos-1;i<size-1;i++){
-        array[i]=array[i+1];
+    cout<<"enter the element you want to insert ";
+    cin>>elem;
+    size++;
+    for (int i=size-1;i>=pos-1;i--){
+        array[i+1]=array[i];
     }
-    size--;
+    array[pos-1]=elem;
     cout<<"printing the new array"<<endl;
     for (int i=0;i<size;i++){
-        cout<<array[i]<<"\t";
+        cout<<array[i]<<" ";
     }
     return 0;
 
